36cfb7ae21dcd27c184c772061e5d5ae0bf088bf
Huang-Wei Chang <chang.huangwei.01@gmail.com>
2014-07-05 10:47:41 -0700

    add sorting

--- site/0.8/js/books.js
+++ site/0.8/js/books.js
@@ -1,16 +1,24 @@
 var app = angular.module('BookInfo', ["firebase", "ngTable"]).
-controller('MyController', ['$firebase', 'ngTableParams', function($firebase, ngTableParams) {
+controller('MyController', ['$filter', '$firebase', 'ngTableParams',
+function($filter, $firebase, ngTableParams) {
     var self = this;
     var refBook = new Firebase("https://booksinfo.firebaseio.com/BooksBrief");
     self.books = $firebase(refBook).asArray();
     self.books.loaded().then(function(){
       self.tableParams = new ngTableParams({
         page: 1,
-        count: 10
+        count: 10,
+        sorting: {
+          name: 'asc' // initial sorting
+        }
       }, {
         total: self.books.length,
         getData: function($defer, params) {
-            $defer.resolve(self.books.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            // use build-in angular filter
+            var orderedData = params.sorting() ?
+                                $filter('orderBy')(self.books, params.orderBy()) :
+                                self.books;
+            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
         }
       });
     });
